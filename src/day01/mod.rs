74d111a8166b3a00412bd6abf54b0b19344398c1
lemmi <lemmi@nerd2nerd.org>
2018-12-02 01:52:13 +0100

    day1

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,8 +1,23 @@
 use std::io;
+use std::collections::HashSet;
 
-pub fn solve(lines: impl Iterator<Item = io::Result<String>>) -> Result<(), std::io::Error> {
-    for l in lines {
-        println!("{}", l?);
+
+pub fn star1(lines: impl Iterator<Item = io::Result<String>>) -> super::StarResult {
+    let s: isize = lines.map(|l| l.unwrap().parse::<isize>().unwrap()).sum();
+    println!("{}", s);
+    Ok(())
+}
+
+pub fn star2(lines: impl Iterator<Item = std::io::Result<String>>) -> super::StarResult {
+    let diffs: Vec<isize> = lines.map(io::Result::unwrap).map(|x| x.parse::<isize>().unwrap()).collect();
+    let freqs = diffs.iter().cycle().scan(0, |state, &x| { *state += x; Some(*state) });
+    let mut seen = HashSet::new();
+
+    for f in freqs {
+        if !seen.insert(f) {
+            println!("{}", f);
+            break;
+        }
     }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use std::io::{self, BufRead};
+use std::fmt::{self, Display, Debug};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufReader, BufRead, Lines};
 
 mod day01;
 
-fn main() ->  Result<(), std::io::Error> {
-    {
-        let stdin = io::stdin();
-        day01::solve(stdin.lock().lines())?;
+pub type StarResult = Result<(), Box<Error>>;
+
+#[derive(Debug)]
+pub struct StarError<T: Display + Debug> {
+    desc: T,
+}
+
+impl<T: Display + Debug> Display for StarError<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.desc)
     }
+}
+
+impl<T: Display + Debug> Error for StarError<T> {
+}
+
+fn main() -> Result<(), Box<Error>> {
+    day01::star1(solve("day01/input")?)?;
+    day01::star2(solve("day01/input")?)?;
     Ok(())
+}
+
+fn solve(input: &str) -> io::Result<Lines<BufReader<File>>> {
+    let file = File::open(input)?;
+    let br = BufReader::new(file);
+    let iter = br.lines();
+    Ok(iter)
 }
